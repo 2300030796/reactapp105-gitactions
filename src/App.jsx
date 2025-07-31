2845241c823a2258c0d502951dee579ab3e2a3da
AKKI <2300030796@kluniversity.in>
2025-07-31 15:16:24 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S105</h1>
-      <h1>CI/CD and Cloud DevOps Course</h1>
+      <h1>CI/CD and Cloud DevOps Course.</h1>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+    
     </div>
   );
 }
